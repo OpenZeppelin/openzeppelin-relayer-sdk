b0158d9b01836e269ea4bb6e8fc4b4979c8e6911
collins-w <delanhype@gmail.com>
2025-03-27 21:19:33 +0300

    Revert "feat: update the changeset release process"
    This reverts commit 283663294c7cd82efbf8b071dd86c8645ef2f8d9.

--- scripts/rerun.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ github, context }) =>
-  github.rest.actions.createWorkflowDispatch({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    workflow_id: 'publish.yml',
-    ref: process.env.REF || process.env.GITHUB_REF_NAME,
-  });
