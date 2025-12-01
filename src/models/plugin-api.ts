db0b488611028a74d0b46366c3c1c9ae0934b8ce
Dylan Kilkenny <dylankilkenny95@gmail.com>
2025-12-01 11:57:09 +0000

    feat: add headers to plugin context type
    Signed-off-by: Dylan Kilkenny <dylankilkenny95@gmail.com>

--- src/models/plugin-api.ts
+++ src/models/plugin-api.ts
@@ -185,12 +185,18 @@ export interface PluginKVStore {
 }
 
 /**
+ * HTTP headers from the incoming request.
+ * Each header name maps to an array of values (since HTTP headers can have multiple values).
+ */
+export type PluginHeaders = Record<string, string[]>;
+
+/**
  * Plugin context with KV always available for modern plugins.
  */
 export interface PluginContext {
   api: PluginAPI;
   kv: PluginKVStore;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  headers: PluginHeaders;
   params: any;
 }
 
