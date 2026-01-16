e7f2f8e9c2c7607552961aed212415b46b1a4821
Collins <delanhype@gmail.com>
2026-01-16 14:22:22 +0300

    fix: fixes build errors (#246)
    fix: fixe build errors

--- src/models/solana-rpc-result.ts
+++ src/models/solana-rpc-result.ts
@@ -1,29 +1,19 @@
 // This file is a workaround to generate the desired output.
 // Workaround for https://github.com/OpenAPITools/openapi-generator/issues/13417
 
-// May contain unused imports in some cases
-// @ts-ignore
+// @ts-expect-error May contain unused imports in some cases
 import type { FeeEstimateResult } from './fee-estimate-result';
-// May contain unused imports in some cases
-// @ts-ignore
+// @ts-expect-error May contain unused imports in some cases
 import type { GetFeaturesEnabledResult } from './get-features-enabled-result';
-// May contain unused imports in some cases
-// @ts-ignore
-import type { GetSupportedTokensItem } from './get-supported-tokens-item';
-// May contain unused imports in some cases
-// @ts-ignore
+// @ts-expect-error May contain unused imports in some cases
 import type { GetSupportedTokensResult } from './get-supported-tokens-result';
-// May contain unused imports in some cases
-// @ts-ignore
+// @ts-expect-error May contain unused imports in some cases
 import type { PrepareTransactionResult } from './prepare-transaction-result';
-// May contain unused imports in some cases
-// @ts-ignore
+// @ts-expect-error May contain unused imports in some cases
 import type { SignAndSendTransactionResult } from './sign-and-send-transaction-result';
-// May contain unused imports in some cases
-// @ts-ignore
+// @ts-expect-error May contain unused imports in some cases
 import type { SignTransactionResult } from './sign-transaction-result';
-// May contain unused imports in some cases
-// @ts-ignore
+// @ts-expect-error May contain unused imports in some cases
 import type { TransferTransactionResult } from './transfer-transaction-result';
 
 type TransferTransactionResultWithMethod = TransferTransactionResult & {
